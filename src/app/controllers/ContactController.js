6342418c88924a23f316726ffacd980b0b57d2e8
Yaba Ernesto <yabaernesto@gmail.com>
2025-08-04 17:06:47 +0100

    feat: rota de edição de contatos adicionados

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -32,7 +32,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email)
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This e-mail is already been token' })
+      return response.status(400).json({ error: 'This e-mail is already in use' })
     }
 
     const contact = await ContactsRepository.create({
@@ -42,8 +42,32 @@ class ContactController {
     return response.json(contact)
   }
 
-  update() {
+  async update(request, response) {
     // editar um registro
+    const { id } = request.params
+    const { name, email, phone, category_id } = request.body
+
+    const contactExists = await ContactsRepository.findById(id)
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found!' })
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required!' })
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email)
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' })
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    })
+
+    return response.json(contact)
   }
 
   async delete(request, response) {
