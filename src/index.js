a5b52a51f3949320ec5544612ca9996950e8cab2
Yaba Ernesto <yabaernesto@gmail.com>
2025-08-04 16:00:44 +0100

    fix: remove route

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ const express = require('express')
 const routes = require('./routes')
 
 const app = express()
-app.use(routes)
 
-app.get('/', (request, response) => {
-  response.send('Hello World')
-})
+app.use(routes)
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
