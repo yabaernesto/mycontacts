462efdfd2586e27080848101159dd64bcc9fd912
Yaba Ernesto <yabaernesto@gmail.com>
2025-08-05 20:26:27 +0100

    feat: conexão do node e postgres

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -3,12 +3,12 @@ CREATE DATABASE mycontacts;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS categories (
-  id UUID NOT NULL UNIQUE DEFAULT uuuid_generate_v4(),
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS contacts (
-  id UUID NOT NULL UNIQUE DEFAULT uuuid_generate_v4(),
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL,
   email VARCHAR UNIQUE,
   phone VARCHAR,
