abcd31b0240a3ad7dd22107b6269a972cc65eb54
Yaba Ernesto <yabaernesto@gmail.com>
2025-08-06 14:39:21 +0100

    feat: método update

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -46,22 +46,14 @@ class ContactsRepository {
     return row
   }
 
-  update(id, { name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id
-      }
+  async update(id, { name, email, phone, category_id }) {
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+    `, [name, email, phone, category_id, id])
 
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ))
-
-      resolve(updatedContact)
-    })
+    return row
   }
 
   delete(id) {
